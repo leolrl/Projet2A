f3aa03c8972b0eaa9fd0ae3a8f57fdb1277c1d5a
Léo Leydour <leo.leydour@gmail.com>
2024-10-11 16:13:06 +0200

    Implementation de la classe AppelTMDB

---
+++ src/Service/AppelTMDB.py
@@ -0,0 +1,47 @@
+import requests
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTdjYzRlYzBjY2VjODAyYjhkODNkZGU4NDkyZDcxZCIsIm5iZiI6MTcyNTYzMzI4NS4wODcxMDMsInN1YiI6IjY2ZGIxMjEwOGJhODA5NTQxNTE2MGM4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._FH9kdpEyOIYMwhcSmatqB-fSXewAqX3uGa6Wj0OIDY"
+    }
+
+class AppelTMDB:
+
+    def __init__(self, header: None):
+        self.header = header
+
+    def obtenir_info_film(self, film_id, language = 'fr-FR'):
+        url = f'https://api.themoviedb.org/3/movie/{film_id}'
+        params = {
+        'language': language
+        }
+        response = requests.get(url, params=params,headers=self.header)
+        data = response.json()
+        return {
+            'Titre': data.get('title', 'Non disponible'),
+            'Titre original': data.get('original_title', 'Non disponible'),
+            'Date de sortie': data.get('release_date', 'Non disponible'),
+            'Durée': f"{data.get('runtime', 'Non disponible')} minutes",
+            'Genre(s)': ', '.join(genre['name'] for genre in data.get('genres', [])),
+            'Résumé': data.get('overview', 'Non disponible'),
+            'Budget': f"{data.get('budget', 0):,} USD",
+            'Revenus': f"{data.get('revenue', 0):,} USD",
+            'Langue originale': data.get('original_language', 'Non disponible'),
+            'Production(s)': ', '.join(production['name'] for production in data.get('production_companies', [])),
+            'Pays de production': ', '.join(production['name'] for production in data.get('production_countries', [])),
+        }
+    
+    def rechercher_film(self, query, language = None, primary_release_year = None, page = None, region = None, year = None):
+        payload = {'query': query,
+                    'language': language,
+                    'primary_release_year': primary_release_year,
+                    'page': page,
+                    'region': region,
+                    'year': year}
+        response = requests.get('https://api.themoviedb.org/3/search/movie?',
+        params=payload, headers=headers)
+        data = response.json()
+        films = data.get('results', [])
+        resultats = [(film['id'], film['title']) for film in films]
+        return resultats
+
