37c9dd117db1ec7136afec0297ede7ddb6593473
Léo Leydour <leo.leydour@gmail.com>
2024-10-11 16:36:07 +0200

    Nouveau endpoint

--- __main__.py
+++ __main__.py
@@ -1,6 +1,11 @@
 from src.Interface.API import run_app
 from src.Service.MovieService import MovieService
 
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTdjYzRlYzBjY2VjODAyYjhkODNkZGU4NDkyZDcxZCIsIm5iZiI6MTcyNTYzMzI4NS4wODcxMDMsInN1YiI6IjY2ZGIxMjEwOGJhODA5NTQxNTE2MGM4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._FH9kdpEyOIYMwhcSmatqB-fSXewAqX3uGa6Wj0OIDY"
+    }
+
 if __name__ == "__main__":
     movie_service = MovieService(None)
     app = run_app(movie_service=movie_service)
--- src/Interface/API.py
+++ src/Interface/API.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI, HTTPException, status
 
 from src.Model.Movie import Movie
 from src.Service.MovieService import MovieService
+from src.Service.AppelTMDB import AppelTMDB
+
+headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTdjYzRlYzBjY2VjODAyYjhkODNkZGU4NDkyZDcxZCIsIm5iZiI6MTcyNTYzMzI4NS4wODcxMDMsInN1YiI6IjY2ZGIxMjEwOGJhODA5NTQxNTE2MGM4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._FH9kdpEyOIYMwhcSmatqB-fSXewAqX3uGa6Wj0OIDY"
+    }
 
 
 def run_app(movie_service: MovieService):
@@ -24,5 +30,10 @@ def run_app(movie_service: MovieService):
             ) from FileNotFoundError
         except Exception as error:
             raise HTTPException(status_code=400, detail="Invalid request") from error
+
+    @app.get("/info/{id_film}")
+    def rechercher_film(id_film):
+        return AppelTMDB(headers).obtenir_info_film(id_film)
+
 
     uvicorn.run(app, port=8000, host="localhost")
